dfd2b36b5fdf861f42d271f13dd8db72ebe7fd87
kylwrld <danielhrsantos06@gmail.com>
2025-10-30 01:39:35 -0300

    feat: add utils package

--- utils/utils.go
+++ utils/utils.go
@@ -1,8 +1,32 @@
 package utils
 
 import (
+	"bufio"
 	"log"
 	"os"
+	"strconv"
 )
 
 var Logger = log.New(os.Stdout, "\033[33m[DEBUG]\033[0m ", 0)
+
+func ReadFile(path string) ([]byte, int) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	bytes := make([]byte, 0)
+	i := 0
+	for scanner.Scan() {
+		bytes = append(bytes, scanner.Bytes()...)
+		bytes = append(bytes, '\n')
+		i++
+	}
+	return bytes, i
+}
+
+func IntToString(num int) string {
+	return strconv.Itoa(num)
+}
